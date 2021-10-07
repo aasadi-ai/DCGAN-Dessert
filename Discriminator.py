435eb4da8e16383ee5050f992278106e8043683e
halifk <aasadi.ai.work@gmail.com>
2021-10-07 15:34:55 -0600

    Adds linear layers and changes position of batchNorm

--- Discriminator.py
+++ Discriminator.py
@@ -8,14 +8,21 @@ class Discriminator(nn.Module):
             self.basicBlock(channels,features),
             self.basicBlock(features,features*2),
             self.basicBlock(features*2,features*4),
-            self.basicBlock(features*4,features*8),
+            nn.Flatten(),
+            nn.Linear(1234,32),
+            nn.Linear(32,16),
+            nn.Linear(16,1),
+            nn.Sigmoid()
         )
 
     def basicBlock(inChannels,outChannels,kernelSize=3,padding=1):
         return nn.Sequential(
             nn.Conv2d(in_channels=inChannels,out_channels=outChannels,kernel_size=kernelSize,padding=padding),
             nn.ReLU(),
-            nn.MaxPool2d(),
-            nn.BatchNorm2d(outChannels)
+            nn.BatchNorm2d(outChannels),
+            nn.MaxPool2d(2)
         )
+
+    def forward(self,X):
+        return self.layers(X)
         
