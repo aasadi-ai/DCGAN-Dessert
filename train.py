d9061f6e3e033fdfe5879b4bbe551a0102c87252
halifk <aasadi.ai.work@gmail.com>
2021-10-07 20:17:44 -0600

    Fixes training loop,tracks losses and accuracy

--- train.py
+++ train.py
@@ -1,33 +1,43 @@
+import torch
 import torch.optim as optim
 import torch.nn as nn
-from Discriminator import Discriminator
+from Models.Discriminator import Discriminator
+from torch.utils.data import DataLoader
+from tennisballDataset import TennisBallDataset
 
 def trainingLoop(model,trainLoader,valLoader,optimizer,criterion,epochs=100):
     trainingLoss = []
     validationLoss = []
     
     for epoch in range(epochs):
+        print(epoch)
+        runningLoss = []
         for imgs,labels in trainLoader:
             optimizer.zero_grad()
             yHat = model(imgs)
-            loss = criterion(yHat,labels)
+            loss = criterion(yHat.squeeze(),labels)
             loss.backward()
             optimizer.step()
-            if epoch%5==0:
-                trainingLoss.append(loss.item())
-                for valImgs,valLabels in valLoader:
-                    optimizer.zero_grad()
-                    yHat = model(valImgs)
-                    valLoss = criterion(yHat,valLabels)
-                    validationLoss.append(valLoss.item())
+            runningLoss.append(loss.item())
+        trainingLoss.append(sum(runningLoss)/len(runningLoss))
+       
+        if epoch%5==0:
+            for valImgs,valLabels in valLoader:
+                optimizer.zero_grad()
+                yHat = model(valImgs).squeeze()
+                valLoss = criterion(yHat,valLabels)
+                validationLoss.append(valLoss.item())
+                prediction = yHat >=0.5
+                print("Accuracy:",10*torch.sum(torch.logical_and(prediction,valLabels))/len(valLabels),"%")
+
     return model,trainingLoss,validationLoss
 
-def train(learningRate=1e-2,epochs=100):
+def train(learningRate=1e-3,epochs=100):
     model = Discriminator(3,16)
     optimizer = optim.SGD(model.parameters(), lr=learningRate)
-    criterion = nn.CrossEntropyLoss()
-    trainLoader = 1
-    valLoader = 1
+    criterion = nn.BCELoss()
+    trainLoader = DataLoader(TennisBallDataset("Data/",(96,96),train=True),batch_size=24)
+    valLoader = DataLoader(TennisBallDataset("Data/",(96,96),train=False),batch_size=100)
     return trainingLoop(model,trainLoader,valLoader,optimizer,criterion,epochs)
     
 
